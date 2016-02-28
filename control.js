c6a170776cc14e42b07d432ab8134c4ef1bb2eca
kenniky <kennikyofficial@gmail.com>
2016-02-28 11:06:03 -0500

    disable buttons temporarily

--- control.js
+++ control.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
 	var firebase = new Firebase('https://glaring-heat-9838.firebaseio.com/');
 
-	$("#but").click(function(e){
-		e.preventDefault();
-		var thing = {
-			"username": $("#name").val()
-		}
-		firebase.push(thing);
-		console.log("hi");
-	});
+	// $("#but").click(function(e){
+	// 	e.preventDefault();
+	// 	var thing = {
+	// 		"username": $("#name").val()
+	// 	}
+	// 	firebase.push(thing);
+	// 	console.log("hi");
+	// });
 
-	$("#clearserver").click(function(e){
-		e.preventDefault();
-		firebase.remove();
-	})
+	// $("#clearserver").click(function(e){
+	// 	e.preventDefault();
+	// 	firebase.remove();
+	// })
 })
