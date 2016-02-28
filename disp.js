e3d3e11ebdfd66b4c7fce843c13cbcd2e8ae7785
kenniky <kennikyofficial@gmail.com>
2016-02-28 10:24:02 -0500

    username?

--- disp.js
+++ disp.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	var firebase = new Firebase('https://glaring-heat-9838.firebaseio.com/');
 
 	function update(t){
-		var para = $("</p>").text(t.action);
+		var para = $("</p>").text("username: "+t.username);
 		$("#placeholder").prepend(para);
 	}
 
