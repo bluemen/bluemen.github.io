c2e4e7978700e2786be2e5ffb0c79da634c037ac
kenniky <kennikyofficial@gmail.com>
2016-02-28 10:47:44 -0500

    s

--- disp.js
+++ disp.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
 	var firebase = new Firebase('https://glaring-heat-9838.firebaseio.com/');
 
 	function update(t){
-		var para = $("</p>").text("username: "+t.username);
+		var para = $("</p>").text("username: "+t);
 		$("#placeholder").prepend(para);
 	}
 
 	firebase.on("child_added", function(snapshot){
-		var input = snapshot.val();
+		var input = snapshot.val().username;
 		update(input);
 	});
 
@@ -17,7 +17,7 @@ $(document).ready(function(){
 
 	$("#x").click(function(e){
 		firebase.once("value",function(snapshot){
-			var thing = snapshot.val();
+			var thing = snapshot.val().x;
 			update(thing);
 		});
 	});
