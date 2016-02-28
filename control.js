1012f4667a5c9a17d1037c33e6a711486b56c907
kenniky <kennikyofficial@gmail.com>
2016-02-28 09:16:44 -0500

    stuff

--- control.js
+++ control.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
 	var firebase = new Firebase('https://glaring-heat-9838.firebaseio.com/');
 
-	$("form").on("#button".onclick,function(e){
+	$("#but").click(function(e){
 		e.preventDefault();
 		var thing = {
 			"action": click
 		}
 		firebase.push(thing);
+		console.log("hi");
 	});
 })
---
+++ disp.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	var firebase = new Firebase('https://glaring-heat-9838.firebaseio.com/');
+
+	function update(t){
+		var para = $("</p>").text(t.action);
+		$("#placeholder").prepend(para);
+	}
+
+	firebase.on("child_added", function(snapshot){
+		var input = snapshot.val();
+		update(input);
+	});
+
+	
+})
