2a44afe17b67d6af40ffe8373085ae1e67856599
kenniky <kennikyofficial@gmail.com>
2016-02-28 09:27:23 -0500

    update

--- control.js
+++ control.js
@@ -2,9 +2,8 @@ $(document).ready(function(){
 	var firebase = new Firebase('https://glaring-heat-9838.firebaseio.com/');
 
 	$("#but").click(function(e){
-		e.preventDefault();
 		var thing = {
-			"action": click
+			"action": "aaaa"
 		}
 		firebase.push(thing);
 		console.log("hi");
