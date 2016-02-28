926018c7162aa5a06b2174fed8975c1fa4777d3d
kenniky <kennikyofficial@gmail.com>
2016-02-28 10:30:45 -0500

    fixed stuff

--- control.js
+++ control.js
@@ -2,8 +2,9 @@ $(document).ready(function(){
 	var firebase = new Firebase('https://glaring-heat-9838.firebaseio.com/');
 
 	$("#but").click(function(e){
+		e.preventDefault();
 		var thing = {
-			"username": $("#name")
+			"username": $("#name").val()
 		}
 		firebase.push(thing);
 		console.log("hi");
